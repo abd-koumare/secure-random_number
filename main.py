ec6f90321bccca01e92e2c299bdcc3a224ee9dc7
abou <funkeeystones@gmail.com>
2020-08-21 16:35:39 +0000

    document the code #1

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from randnum import generate_random_num_str
 
 
 if __name__ == '__main__':
-    rand_number = generate_random_num_str()
-    print(f'{rand_number} default length: {len(rand_number)}')
 
     for x in range(10, 20):
-        rand_number = generate_random_num_str(x)
+        rand_number = generate_random_num_str()
         print(f'{rand_number} length: {len(rand_number)}')
